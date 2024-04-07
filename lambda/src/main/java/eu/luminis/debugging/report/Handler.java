394f45d29ff5b006d93954724881993f81feaa71
Nico Krijnen <nico.krijnen@luminis.eu>
2024-04-07 15:16:13 +0200

    Switch to crt http client

--- lambda/src/main/java/eu/luminis/debugging/report/Handler.java
+++ lambda/src/main/java/eu/luminis/debugging/report/Handler.java
@@ -3,14 +3,21 @@ package eu.luminis.debugging.report;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import software.amazon.awssdk.http.crt.AwsCrtHttpClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
+import java.time.Duration;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class Handler implements RequestHandler<SNSEvent, String> {
 
-    private S3Client s3 = S3Client.builder().build();
+    private S3Client s3 = S3Client.builder()
+            .httpClientBuilder(AwsCrtHttpClient.builder()
+                    .connectionTimeout(Duration.ofSeconds(3))
+                    .maxConcurrency(100)
+            )
+            .build();
 
     @Override
     public String handleRequest(SNSEvent event, Context context) {
