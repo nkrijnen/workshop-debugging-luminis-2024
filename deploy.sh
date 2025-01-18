80b4882d9eff28ed4cf7f81e38dac07a066f5e78
Nico Krijnen <nico.krijnen@luminis.eu>
2025-01-18 15:38:35 +0100

    Add commented sample to speedup lambda-only deployments

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export AWS_PROFILE=AWSAdministratorAccess-998150297714
 ./gradlew shadowJar
-#(cd infra; cdk deploy --require-approval never --hotswap-fallback)
+#(cd infra; cdk deploy --require-approval never --hotswap-fallback --watch)
 (cd infra; cdk deploy --require-approval never)
