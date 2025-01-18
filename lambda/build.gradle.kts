aa109d7fa4abec0aba59a3006a3db18d760a4816
Nico Krijnen <nico.krijnen@luminis.eu>
2025-01-18 15:14:04 +0100

    Add missing slf4j config and fix shadow log4j2 config

--- lambda/build.gradle.kts
+++ lambda/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
+tasks {
+    shadowJar {
+        transform(com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer::class.java)
+    }
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
@@ -21,7 +27,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("com.amazonaws:aws-lambda-java-log4j2:1.6.0")
-
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.3")
 }
 
 application {
