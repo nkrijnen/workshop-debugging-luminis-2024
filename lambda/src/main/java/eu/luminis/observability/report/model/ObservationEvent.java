4749e0871f18899d4b8bc70a1e00688df0d39c1b
Nico Krijnen <nico.krijnen@luminis.eu>
2025-12-08 10:57:40 +0100

    Add scenario to observation event
    To help debug cases where lamda still shortly gets triggered with previous topic

--- lambda/src/main/java/eu/luminis/observability/report/model/ObservationEvent.java
+++ lambda/src/main/java/eu/luminis/observability/report/model/ObservationEvent.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public record ObservationEvent(
         List<Observation> observations,
-        LocalDateTime date
+        LocalDateTime date,
+        int scenario
 ) {
 }
--- lambda/src/test/java/eu/luminis/observability/report/util/JsonTest.java
+++ lambda/src/test/java/eu/luminis/observability/report/util/JsonTest.java
@@ -14,22 +14,37 @@ class JsonTest {
             List.of(
                     new Observation("Bla", 10, 50)
             ),
-            LocalDateTime.of(2024, 4, 8, 13, 54, 32)
+            LocalDateTime.of(2024, 4, 8, 13, 54, 32),
+            5
     );
 
     @Test
     void format() {
         String json = Json.format(sampleEvent);
 
-        assertEquals("{\"observations\":[{\"station\":\"Bla\",\"temperature\":10.0,\"humidity\":50.0}],\"date\":\"2024-04-08T13:54:32\"}", json);
+        assertEquals("{\"observations\":[{\"station\":\"Bla\",\"temperature\":10.0,\"humidity\":50.0}],\"date\":\"2024-04-08T13:54:32\",\"scenario\":5}", json);
     }
 
     @Test
     void parse() {
-        String json = "{\"observations\":[{\"station\":\"Bla\",\"temperature\":10.0,\"humidity\":50.0}],\"date\":\"2024-04-08T13:54:32\"}";
+        String json = "{\"observations\":[{\"station\":\"Bla\",\"temperature\":10.0,\"humidity\":50.0}],\"date\":\"2024-04-08T13:54:32\",\"scenario\":5}";
 
         ObservationEvent event = Json.parse(json, ObservationEvent.class);
 
         assertEquals(sampleEvent, event);
+    }
+
+    @Test
+    void parseWithoutScenarioInBodyAsThatWasAddedLater() {
+        String json = "{\"observations\":[{\"station\":\"Bla\",\"temperature\":10.0,\"humidity\":50.0}],\"date\":\"2024-04-08T13:54:32\"}";
+
+        ObservationEvent actualEvent = Json.parse(json, ObservationEvent.class);
+
+        ObservationEvent expectedEvent = new ObservationEvent(
+                sampleEvent.observations(),
+                sampleEvent.date(),
+                0 // default value for scenario
+        );
+        assertEquals(expectedEvent, actualEvent);
     }
 }
